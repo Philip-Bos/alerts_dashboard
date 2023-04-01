ac2d35786269a96b36fd8a8b248c8a2ef9c1aa3d
Philip Bos <104074018+Philip-Bos@users.noreply.github.com>
2023-04-01 15:09:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import os
 
     os.environ["SERVER_PROTOCOL"] = "HTTP/1.1"
-    //TODO replace secret key with env variable
+    #TODO replace secret key with env variable
     app.secret_key = "super secret"
     app.run()
